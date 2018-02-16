f4f0e77ece5ca85a0199dfbc4e47bd66261a4dfe
Nikita ⌁ Golodenko <nikita@golodenko.com>
2018-02-16 16:51:59 +0200

    rubocop corrections

--- spec/models/import_spec.rb
+++ spec/models/import_spec.rb
@@ -11,7 +11,7 @@ describe 'Import' do
         import = FactoryBot.create(:import)
         allow_any_instance_of(Paperclip::Attachment).to receive(:path).and_return(SAMPLE_CSV_FILE_PATH)
 
-        expect{import.import_products(CSVProductParser)}.to change(Spree::Product, :count).from(21).to(24)
+        expect { import.import_products(CSVProductParser) }.to change(Spree::Product, :count).from(21).to(24)
       end
     end
 
@@ -20,7 +20,7 @@ describe 'Import' do
         import = FactoryBot.create(:import)
         allow_any_instance_of(Paperclip::Attachment).to receive(:path).and_return(EMPTY_SAMPLE_CSV_FILE_PATH)
 
-        expect{import.import_products(CSVProductParser)}.not_to change(Spree::Product, :count)
+        expect { import.import_products(CSVProductParser) }.not_to change(Spree::Product, :count)
       end
     end
   end
